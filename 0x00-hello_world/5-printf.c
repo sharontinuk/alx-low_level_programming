2d66859cd8470d52ef31cd848cb14f398c6bd271
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2022-09-08 22:08:26 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main -Entry point
+ * main-Entry point
  *
  * Return: always 0
  */
@@ -11,6 +11,6 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	
-	retun (0);
+	return (0);
 	
 }
