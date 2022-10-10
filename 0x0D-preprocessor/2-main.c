48edfac428b8efa65d328fd9714471f75874389f
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2022-10-10 18:33:26 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-	
 /**
-* main - prints the name of the file
-*
-* Return: always 0
-*/
-	
+ * main - Program compilation starts from the main
+ * Description: this program prints the name of the file it was comipiled from
+ * author: sharontinuk
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
